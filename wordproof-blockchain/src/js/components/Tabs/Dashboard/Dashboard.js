6844a95acf111d110889774700e326956f5a5e88
Marijn Bent <marijnbent@gmail.com>
2019-05-13 14:58:53 +0200

    Indentation

--- wordproof-blockchain/src/js/components/Tabs/Dashboard/Dashboard.js
+++ wordproof-blockchain/src/js/components/Tabs/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 export default class Dashboard extends Component {
   constructor(props) {
@@ -31,51 +31,65 @@ export default class Dashboard extends Component {
 
   closePopup = () => {
     window.localStorage.setItem('wordproof-has-account', 'true')
-    this.setState({ hasAccount: true })
+    this.setState({hasAccount: true})
   }
 
 
   render() {
     return (
       <div>
-        { !this.state.hasAccount ?
+        {!this.state.hasAccount ?
           <div className="vo-card">
-		  	<h3>Welcome, WordProof tester!</h3>
-			<button type="button" className="button dismiss" onClick={this.closePopup}><span className="dashicons dashicons-no-alt"></span></button>
-			<iframe width="560" height="315" src="https://www.youtube.com/embed/5GUlcfaKsyk" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-			<br /><br />
+            <h3>Welcome, WordProof tester!</h3>
+            <button type="button" className="button dismiss" onClick={this.closePopup}><span
+              className="dashicons dashicons-no-alt"></span></button>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/5GUlcfaKsyk" frameBorder="0"
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen></iframe>
+            <br/><br/>
             <label className="label">Do you already have an EOS or Telos blockchain account?</label>
-			<ul className="bullet-list">
+            <ul className="bullet-list">
               <li><a href="#create-telos-account">No, I want to create a free Telos account</a></li>
               <li><a href="#create-eos-account">No, I want to buy an EOS account</a></li>
               <li><a href="#" onClick={this.handleYes}>Yes (select the blockchain you want to use below)</a></li>
-			</ul>
+            </ul>
           </div> : ''
         }
 
         <div className="vo-card">
-            <div className="form-group">
-              <label className="label" id="label_wordproof_network" htmlFor="wordproof_network">In what blockchain do you want to timestamp your content?</label>
-              <select name="wordproof_network" id="wordproof_network" value={this.state.network} onChange={this.handleNetwork}>
-                <option value="eos_main">EOS</option>
-                <option value="telos_main">Telos</option>
-                <option value="eos_jungle">EOS Jungle Testnet</option>
-              </select>
-            </div>
+          <div className="form-group">
+            <label className="label" id="label_wordproof_network" htmlFor="wordproof_network">In what blockchain do you
+              want to timestamp your content?</label>
+            <select name="wordproof_network" id="wordproof_network" value={this.state.network}
+                    onChange={this.handleNetwork}>
+              <option value="eos_main">EOS</option>
+              <option value="telos_main">Telos</option>
+              <option value="eos_jungle">EOS Jungle Testnet</option>
+            </select>
+          </div>
 
-            <div className="form-group">
-              <label htmlFor="" className="label">What information do you want to store on the blockchain?</label>
-              <input type="checkbox" name="" id="" checked disabled/> A hash of <em>the_title()</em> and <em>the_content()</em> in a memo <br/>
-              <input type="checkbox" name="wordproof_store_content" checked={this.state.storeContent} onChange={this.handleStoreContent}/> <em>the_content()</em> <br/>
-              <input type="checkbox" name="wordproof_store_ram" checked={this.state.storeRam} onChange={this.handleStoreRam}/> A hash of <em>the_title()</em> and <em>the_content()</em> in RAM (WARNING: costs you RAM for every time-stamp)
-            </div>
+          <div className="form-group">
+            <label htmlFor="" className="label">What information do you want to store on the blockchain?</label>
+            <input type="checkbox" name="" id="" checked disabled/> A hash
+            of <em>the_title()</em> and <em>the_content()</em> in a memo <br/>
+            <input type="checkbox" name="wordproof_store_content" checked={this.state.storeContent}
+                   onChange={this.handleStoreContent}/> <em>the_content()</em> <br/>
+            <input type="checkbox" name="wordproof_store_ram" checked={this.state.storeRam}
+                   onChange={this.handleStoreRam}/> A hash of <em>the_title()</em> and <em>the_content()</em> in RAM
+            (WARNING: costs you RAM for every time-stamp)
+          </div>
 
-            <div className="form-group">
-              <label htmlFor="" className="label" title="CERTIFICATE_URL will be replaced by the URL to the WordProof certificate.">How do you want to refer to the WordProof blockchain certificate? </label>
-              <input type="text" className="textinput" name="wordproof_certificate_template" value={this.state.certificateTemplate} onChange={e => this.setState({ certificateTemplate: e.target.value })}/>
-            </div>
+          <div className="form-group">
+            <label htmlFor="" className="label"
+                   title="CERTIFICATE_URL will be replaced by the URL to the WordProof certificate.">How do you want to
+              refer to the WordProof blockchain certificate? </label>
+            <input type="text" className="textinput" name="wordproof_certificate_template"
+                   value={this.state.certificateTemplate}
+                   onChange={e => this.setState({certificateTemplate: e.target.value})}/>
+          </div>
 
-            <input type="submit" name="submit" id="submit" className="button button-primary" value={wordproofSettings.saveChanges}/>
+          <input type="submit" name="submit" id="submit" className="button button-primary"
+                 value={wordproofSettings.saveChanges}/>
         </div>
       </div>
     )
--- wordproof-blockchain/src/js/components/Tabs/EOS/EOS.js
+++ wordproof-blockchain/src/js/components/Tabs/EOS/EOS.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 export default class EOS extends Component {
   render() {
@@ -7,23 +7,27 @@ export default class EOS extends Component {
         <div className="vo-card">
           <h3>Step 1: Buy an EOS account</h3>
           <p>
-            To time-stamp your content, you need an EOS account (or a <a href="#create-telos-account">free Telos account</a>). Buy your EOS account on EOS Name Service. Here’s how:
+            To time-stamp your content, you need an EOS account (or a <a href="#create-telos-account">free Telos
+            account</a>). Buy your EOS account on EOS Name Service. Here’s how:
           </p>
 
-		  <iframe width="560" height="315" src="https://www.youtube.com/embed/rQkrrLXjZH8" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/rQkrrLXjZH8" frameBorder="0"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen></iframe>
 
           <p>
             Now, import your new private key into one of these EOS wallets:
           </p>
 
-		  <h3>Step 2: Created your account? Time to get a wallet!</h3>
+          <h3>Step 2: Created your account? Time to get a wallet!</h3>
           <ul className="bullet-list">
             <li><a href="https://get-scatter.com/" target="_blank" rel="noopener noreferrer">Scatter</a></li>
           </ul>
 
-		  <p>
-		    Please join the <a href="https://t.me/WordProof" target="_blank">WordProof Telegram</a> for questions, suggestions or support related items.
-		  </p>
+          <p>
+            Please join the <a href="https://t.me/WordProof" target="_blank" rel="noopener noreferrer" >WordProof Telegram</a> for questions,
+            suggestions or support related items.
+          </p>
 
         </div>
       </div>

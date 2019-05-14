874474d9fc911c27a346a690730de327222eba95
Marijn Bent <marijnbent@gmail.com>
2019-05-14 17:49:25 +0200

    Change support page

--- wordproof-timestamp/src/js/components/Tabs/Support/Support.js
+++ wordproof-timestamp/src/js/components/Tabs/Support/Support.js
@@ -11,10 +11,14 @@ export default class Configure extends Component {
       <div>
         <div className="vo-card">
           <h3>Need any help?</h3>
-          <p>Thank you for using the WordProof Timestamp plugin. Even though we try to make everything simple, we are
-            always ready to help if there are any questions.</p>
-          <a href="https://t.me/WordProof" target="_blank" rel="noopener noreferrer">Join our Telegram group</a> <strong>(Recommended)</strong><br/>
-          <a href="mailto:info@wordproof.io" target="_blank" rel="noopener noreferrer">Send an email</a>
+          <p>Thank you for using the WordProof Timestamp plugin! Blockchain is a difficult technology which requires the set-up of a wallet. This can be a daunting process, but we have a great community to help you out. The WordProof community is active in our Telegram groups, where we talk about new features and help each other. Please join these groups for any support requests:</p>
+          <ul>
+            <li><a href="https://t.me/joinchat/DVuJAEfhf2QURBBjOWc2XA" target="_blank" rel="noopener noreferrer">Telegram User Group</a></li>
+            <li><a href="https://t.me/WordProof" target="_blank" rel="noopener noreferrer">Telegram Community Group</a></li>
+          </ul>
+          <p>For other inquiries, <a href="mailto:info@wordproof.io" target="_blank" rel="noopener noreferrer">Send an email</a>.</p>
+          <h3>Credits</h3>
+          <p><a href="https://wordproof.io/wordpress-plugin-wordproof/credits/" target="_blank" rel="noopener noreferrer">WordProof Timestamp plugin Credits</a></p>
         </div>
       </div>
     )

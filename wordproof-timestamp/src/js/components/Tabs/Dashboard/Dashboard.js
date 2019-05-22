fd9a8ba82b26de66ae14b7eb627cec468bc3a75b
Marijn Bent <marijnbent@gmail.com>
2019-05-22 15:25:12 +0200

    Add Dashboard content

--- wordproof-timestamp/src/js/components/Tabs/Dashboard/Dashboard.js
+++ wordproof-timestamp/src/js/components/Tabs/Dashboard/Dashboard.js
@@ -7,22 +7,27 @@ export default class Dashboard extends Component {
     }
   }
 
+  handleWindowPopup = (event, url) => {
+    event.preventDefault();
+    window.open(
+      url,
+      'popUpWindow',
+      'height=600,width=900,left=50,top=50,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes'
+    );
+  }
+
   render() {
     return (
       <div>
         {!this.state.hasAccount ?
           <div className="vo-card vo-columns">
             <div className="vo-col">
+              <p>Welcome to the WordProof Timestamp plugin! WordProof brings the benefits of blockchain to WordPress. If you are new, head to the Setup to get started!</p>
               <h3>What is WordProof Timestamp?</h3>
-              <p>Welcome to the WordProof - Time-stamping beta plugin!:) We are thrilled to have you here as a tester. WordProof is the bridge between WordPress and Blockchain: we bring the benefits of blockchain to the WordPress ecosystem via various plugins.</p>
-              <p>WordProof Time-stamping lets you time-stamp your content onto the EOS.IO ecosystem. You can choose between two blockchains:</p>
-              <ul className="bullet-list">
-                <li>Telos blockchain (free!)</li>
-                <li>EOS blockchain</li>
-              </ul>
-              <p>After creating an account on one of the two blockchains above, you will need to download Scatter, which is a wallet that runs on your computer. We will walk you through every step of the way, no worries!</p>
-              <br/>
-              <a href="#configure" className="button button-primary">Go to Step 1: Configure</a>
+              <p>WordProof Timestamp lets you timestamp your content on the blockchain. By doing so, you claim ownership and responsibility (it is impossible for someone to claim they were first, since the URL, date and content is published to the blockchain). In addition, you show transparency to your visitors: this content is real and has not been tampered with (or it would be visible in the blockchain!).</p>
+              <h3>One time setup</h3>
+              <p>The Set-Up takes 5 minutes: 1) Create a blockchain account 2) Download a ‘Wallet’ 3) Configure the Wallet. You only need to do the set-up once!</p>
+              <button className="button button-primary" onClick={(e) => this.handleWindowPopup(e, 'https://wordproof.io/timestamp-setup-wizard')}>Launch Setup Wizard</button>
             </div>
             <div className="vo-col">
               <iframe width="560" height="315" src="https://www.youtube.com/embed/5GUlcfaKsyk" frameBorder="0"

b86f56f992fa0310e8a2ee32d2e730b5415fdb46
Marijn Bent <marijnbent@gmail.com>
2019-05-23 22:39:30 +0200

    Use bulma modal

--- wordproof-timestamp/src/js/components/Popup/Popup.js
+++ wordproof-timestamp/src/js/components/Popup/Popup.js
@@ -16,35 +16,33 @@ export default class Popup extends Component {
     return (
       <ShadowDOM include={`${wordproofData.wordProofCssDir}/frontend.css`}>
         <div className="shadowHost">
-          <div className="wordproof-popup is-active">
-            <div className="overlay">
-              <div className="modal">
-                {/*<a className="modal-close" href="#">&times;</a>*/}
-                <div className="modal-header">
 
-                </div>
-                <div className="modal-content">
-                  <h2>Blockchain Certificate</h2>
-                  <h3>Validated by WordProof</h3>
+          <div className="modal">
+            <div className="modal-background"></div>
+            <div className="modal-card">
+              <header className="modal-card-head"></header>
+              <section className="modal-card-body">
+                <h2 className="title has-text-centered">Blockchain Certificate</h2>
+                <h3 className="subtitle has-text-centered">Validated by WordProof</h3>
 
-                  <section className="mockup-browser" data-url="http://www.bowie-to-bowie.io/this-is-the-url">
-                    <div className="mockup-browser-content">
-                      <h3>hi there</h3>
-                      <p>Hehehe</p>
-                    </div>
-                    <div className="mockup-browser-footer">
-                      <p>content</p>
-                    </div>
+                <section className="mockup-browser" data-url="http://www.bowie-to-bowie.io/this-is-the-url">
+                  <div className="mockup-browser-content">
+                    <h3>hi there</h3>
+                    <p>Hehehe</p>
+                  </div>
+                  <div className="mockup-browser-footer">
+                    <p>content</p>
+                  </div>
+                </section>
 
-                  </section>
-
-                </div>
-                <div className="modal-footer">
-
-                </div>
-              </div>
+              </section>
+              <footer className="modal-card-foot">
+                <button className="button is-success">Save changes</button>
+                <button className="button">Cancel</button>
+              </footer>
             </div>
           </div>
+
         </div>
       </ShadowDOM>
     )

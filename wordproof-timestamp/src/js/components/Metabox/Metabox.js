083296e85f4d3e71a5fe88ed84c550abab13885d
Marijn Bent <marijn@marijnbent.nl>
2019-05-27 14:57:25 +0200

    Refactor

--- wordproof-timestamp/src/js/components/Metabox/Metabox.js
+++ wordproof-timestamp/src/js/components/Metabox/Metabox.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import initWallet from '../../wallet'
 import timestamp from '../../timestamp'
 
@@ -25,26 +25,30 @@ export default class Metabox extends Component {
     this.startLoading();
     const wallet = await this.getWallet();
     try {
-      if (!wallet.connected) { await wallet.connect() }
-      if (!wallet.authenticated) { await wallet.login() }
+      if (!wallet.connected) {
+        await wallet.connect()
+      }
+      if (!wallet.authenticated) {
+        await wallet.login()
+      }
       timestamp(wallet).then(response => response.json())
-          .then((result) => {
-            console.log(result);
-            if (result.success) {
-              this.stopLoading();
-              this.setState({
-                success: true,
-                successData: result.data
-              });
-            }
-          }) // JSON-string from `response.json()` call
-          .catch(error => {
-            this.stopLoading();
-            console.error(error);
+      .then((result) => {
+        console.log(result);
+        if (result.success) {
+          this.stopLoading();
+          this.setState({
+            success: true,
+            successData: result.data
           });
+        }
+      }) // JSON-string from `response.json()` call
+      .catch(error => {
+        this.stopLoading();
+        console.error(error);
+      });
     } catch (error) {
       this.stopLoading();
-      this.setState({failed:true});
+      this.setState({failed: true});
       console.log(error);
     }
   }
@@ -52,9 +56,11 @@ export default class Metabox extends Component {
   handleDisconnectClick = async () => {
     const wallet = await this.getWallet()
     try {
-      if (!wallet.connected) { await wallet.connect() }
+      if (!wallet.connected) {
+        await wallet.connect()
+      }
       await wallet.terminate()
-      this.setState({failed:false})
+      this.setState({failed: false})
     } catch (error) {
       console.log(error)
     }
@@ -74,7 +80,9 @@ export default class Metabox extends Component {
       const wallet = initWallet();
       try {
         await wallet.connect();
-        if (!wallet.authenticated) { await wallet.login(); }
+        if (!wallet.authenticated) {
+          await wallet.login();
+        }
         this.setState({
           wallet: wallet,
           disabled: false
@@ -86,33 +94,43 @@ export default class Metabox extends Component {
     }
     return this.state.wallet;
   }
+
   render() {
     return (
       <div className="wordproof-metabox">
-        { this.state.noScatter ? 
-          <p>No EOSIO wallet detected. Open you wallet or <a href={wordproofData.settingsURL}>download an EOSIO wallet now</a>.</p> : ''
+        {this.state.noScatter ?
+          <p>No EOSIO wallet detected. Open you wallet or <a href={wordproofData.settingsURL}>download an EOSIO wallet
+            now</a>.</p> : ''
         }
 
-        { this.state.failed ? 
-           <p>No EOSIO wallet detected. Open you wallet or <a href={wordproofData.settingsURL}>download an EOSIO wallet now</a>.</p> : ''
+        {this.state.failed ?
+          <p>No EOSIO wallet detected. Open you wallet or <a href={wordproofData.settingsURL}>download an EOSIO wallet
+            now</a>.</p> : ''
         }
 
-        { this.state.wallet && this.state.wallet.authenticated ?
+        {this.state.wallet && this.state.wallet.authenticated ?
           <div>Logged in as <strong>{this.state.wallet.accountInfo.account_name}</strong></div> : ''
         }
 
-        { this.state.disabled ?
-          <div className="connecting-to-wallet"><img className="loading-spinner" height="64px" width="64px" src="/wp-admin/images/spinner-2x.gif" alt="loading" />Connecting...</div>: ''
+        {this.state.disabled ?
+          <div className="connecting-to-wallet"><img className="loading-spinner" height="64px" width="64px"
+                                                     src="/wp-admin/images/spinner-2x.gif" alt="loading"/>Connecting...
+          </div> : ''
         }
 
         <br/>
-        { !this.state.success ? 
+        {!this.state.success ?
           <div>
-            <button type="button" className={`button ${this.state.loadingClass}`} onClick={this.handleClick} disabled={this.state.disabled}>Timestamp Post</button>
-            <button type="button" className={`button ${this.state.loadingClass}`} onClick={this.handleDisconnectClick} disabled={this.state.disabled}>Disconnect</button>
+            <button type="button" className={`button ${this.state.loadingClass}`} onClick={this.handleClick}
+                    disabled={this.state.disabled}>Timestamp Post
+            </button>
+            <button type="button" className={`button ${this.state.loadingClass}`} onClick={this.handleDisconnectClick}
+                    disabled={this.state.disabled}>Disconnect
+            </button>
           </div>
           :
-          <p>Success! <a target="_blank" rel="noopener noreferrer" href={this.state.successData.url}>Link to proof</a></p>
+          <p>Success! <a target="_blank" rel="noopener noreferrer" href={this.state.successData.url}>Link to proof</a>
+          </p>
         }
       </div>
     )

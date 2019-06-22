f19ec611b6c312644a41f28af4278b51da95c674
Marijn Bent <marijn@marijnbent.nl>
2019-06-22 11:07:57 +0200

    Change notification style

--- wordproof-timestamp/includes/NotificationHelper.php
+++ wordproof-timestamp/includes/NotificationHelper.php
@@ -70,7 +70,7 @@ class NotificationHelper
 
   private function connectToWallet()
   {
-    $message = 'It looks like you started the WordProof Setup Wizard, but did not timestamp yet! Join our <a href="https://t.me/joinchat/DVuJAEfhf2QURBBjOWc2XA">Telegram group</a> for help, or send us an <a href="https://wordproof.io/contact">email</a>.';
+    $message = 'It looks like you started the WordProof Setup Wizard, but did not timestamp yet! Join our <a target="_blank" rel="noopener noreferrer" href="https://t.me/joinchat/DVuJAEfhf2QURBBjOWc2XA">Telegram group</a> for help, or send us an <a href="https://wordproof.io/contact">email</a>.';
     $type = 'info';
     echo self::getNotificationTemplate($type, $message);
   }
@@ -92,10 +92,10 @@ class NotificationHelper
   private function getNotificationTemplate($type, $message, $buttonText = '', $buttonLink = '', $isDismissible = false)
   {
     ob_start(); ?>
-      <div class="notice wordproof-notice wordproof-notice--<?php echo $type; ?> <?php echo ($isDismissible) ? 'is-dismissible' : ''; ?>">
+      <div class="notice notice-<?php echo $type; ?> <?php echo ($isDismissible) ? 'is-dismissible' : ''; ?>">
           <p><?php _e($message, WORDPROOF_SLUG); ?></p>
         <?php if (!empty($buttonLink)): ?>
-            <p><a href="<?php echo $buttonLink; ?>"><?php _e($buttonText, WORDPROOF_SLUG); ?></a></p>
+            <p><a target="_blank" rel="noopener noreferrer" href="<?php echo $buttonLink; ?>"><?php _e($buttonText, WORDPROOF_SLUG); ?></a></p>
         <?php endif; ?>
       </div>
     <?php

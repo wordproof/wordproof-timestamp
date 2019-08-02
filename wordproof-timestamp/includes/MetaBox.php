719d1294da2601e3da38fac6e39a3054e7d5846f
Marijn Bent <marijn@marijnbent.nl>
2019-08-02 17:02:26 +0200

    Remove metabox if wsfy is active

--- wordproof-timestamp/includes/MetaBox.php
+++ wordproof-timestamp/includes/MetaBox.php
@@ -10,7 +10,11 @@ class MetaBox {
 
     public function __construct() {
         if (current_user_can('manage_options')) {
+          $options = get_option('wordproof_wsfy');
+
+          if (!isset($options['active']) || $options['active'] === false) {
             add_action('add_meta_boxes', array($this, 'addMetaBox'));
+          }
         }
     }
 

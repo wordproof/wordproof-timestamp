85ce000edcf0a15b4cf3f1b4b08af1a1440223a6
Marijn Bent <marijn@marijnbent.nl>
2019-08-22 23:14:13 +0200

    Remove timestamp from database

--- wordproof-timestamp/includes/Page/AutoStampPage.php
+++ wordproof-timestamp/includes/Page/AutoStampPage.php
@@ -10,7 +10,6 @@ class AutoStampPage {
 
     public function __construct() {
         add_action('admin_menu', array($this, 'addSettingsPage'));
-	    add_action('admin_post_wordproof_form_action', array($this, 'saveSettings'));
     }
 
     public function addSettingsPage() {
@@ -26,32 +25,29 @@ class AutoStampPage {
 
     public function generateSettingsPage() {
         $cpt = 'post';
+      $offset = 0;
+      $force = false;
+
+        if (isset($_GET['remove_timestamp']) && !empty($_GET['remove_timestamp'])) {
+            self::removeTimestamp(intval($_GET['remove_timestamp']));
+        }
+
         if (isset($_GET['cpt']) && !empty($_GET['cpt'])) {
             $cpt = sanitize_text_field($_GET['cpt']);
         }
 
-        $metaQueries = ['key' => 'wordproof_timestamp_data', 'compare' => 'NOT EXISTS'];
-        $offset = 0;
-        if (isset($_GET['force']) && isset($_GET['offset'])) {
+      if (isset($_GET['force']) && isset($_GET['offset'])) {
             $offset = intval($_GET['offset']);
-            $metaQueries = [];
+            $force = true;
         }
 
-        $args = [
-            'post_status' => 'publish',
-            'post_type' => $cpt,
-            'numberposts' => 1000,
-            'offset' => $offset,
-            'meta_query' => [$metaQueries]
-        ];
-        $posts = get_posts($args);
+      $posts = self::getPosts($cpt, $offset, $force);
+      wp_localize_script('wordproof.admin.js', 'wordproofAutoStamp', [
+        'posts' => $posts,
+        'loading' => admin_url() . 'images/spinner-2x.gif'
+      ]);
 
-        wp_localize_script('wordproof.admin.js', 'wordproofAutoStamp', [
-            'posts' => $posts,
-            'loading' => admin_url() . 'images/spinner-2x.gif'
-        ]);
-
-        ?>
+      ?>
         <div class="wrap">
             <h1>WordProof Settings</h1>
             <p>Start auto-stamping <?php echo sizeof($posts); ?> posts which are not timestamped yet. We will skip posts that are stamped already. <strong>Please make sure your setup works beforehand.</strong></p>
@@ -65,41 +61,24 @@ class AutoStampPage {
         <?php
     }
 
-    public function saveSettings()
-    {
-      if (isset($_POST['wordproof_admin_form_nonce']) && wp_verify_nonce($_POST['wordproof_admin_form_nonce'], 'wordproof_admin_form_nonce')) {
-
-          if (isset($_POST['wordproof_network'])) {
-          $value = sanitize_text_field($_POST['wordproof_network']);
-          update_option('wordproof_network', $value);
-        }
-
-        if (isset($_POST['wordproof_certificate_text'])) {
-          $value = sanitize_text_field($_POST['wordproof_certificate_text']);
-          update_option('wordproof_certificate_text', $value);
-        }
-
-        if (isset($_POST['wordproof_certificate_dom_selector'])) {
-          $value = sanitize_text_field($_POST['wordproof_certificate_dom_selector']);
-          update_option('wordproof_certificate_dom_selector', $value);
-        }
-
-        if (isset($_POST['wsfy_settings'])) {
-          $post = $_POST['wsfy_settings'];
-          $accessToken = sanitize_text_field($post['access_token']);
-          $siteId = sanitize_text_field($post['site_id']);
-          $revisions = isset($post['no_revisions']) ? true : false;
-
-          if (empty($accessToken) || empty($siteId)) {
-            $options = ['accessToken' => $accessToken, 'siteId' => $siteId, 'active' => false, 'noRevisions' => $revisions];
-          } else {
-            $options = ['accessToken' => $accessToken, 'siteId' => $siteId, 'active' => true, 'noRevisions' => $revisions];
-          }
-          update_option('wordproof_wsfy', $options);
-        }
-
-      }
-      wp_redirect(admin_url('admin.php?page=wordproof'));
-      die();
+    public function removeTimestamp($id) {
+        delete_post_meta($id, 'wordproof_timestamp_data');
     }
+
+    public function getPosts($cpt = 'post', $offset = 0, $force = false) {
+
+      $metaQueries = ['key' => 'wordproof_timestamp_data', 'compare' => 'NOT EXISTS'];
+      if ($force)
+        $metaQueries = [];
+
+      $args = [
+        'post_status' => 'publish',
+        'post_type' => $cpt,
+        'numberposts' => 1000,
+        'offset' => $offset,
+        'meta_query' => [$metaQueries]
+      ];
+      return $posts = get_posts($args);
+    }
+
 }

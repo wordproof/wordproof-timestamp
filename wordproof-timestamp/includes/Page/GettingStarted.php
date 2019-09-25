fe67f1a5da677d73a0bf13beac70de44d51639cf
Marijn Bent <marijn@marijnbent.nl>
2019-09-25 17:59:38 +0200

    Getting started layout

--- wordproof-timestamp/includes/Page/GettingStarted.php
+++ wordproof-timestamp/includes/Page/GettingStarted.php
@@ -28,6 +28,7 @@ class GettingStarted {
   }
 
     public function generateSettingsPage() {
+      wp_enqueue_style('wordproof.getting-started.css', WORDPROOF_URI_CSS . '/getting-started.css', array(), filemtime(WORDPROOF_DIR_CSS . '/getting-started.css'));
       wp_enqueue_script('wordproof.getting-started.js', WORDPROOF_URI_JS . '/getting-started.js', array(), filemtime(WORDPROOF_DIR_JS . '/getting-started.js'), true);
       wp_localize_script('wordproof.getting-started.js', 'wordproof', []);
 
--- wordproof-timestamp/src/js/components/GettingStarted/GettingStarted.js
+++ wordproof-timestamp/src/js/components/GettingStarted/GettingStarted.js
@@ -13,8 +13,22 @@ class GettingStarted extends Component {
 
   render() {
     return (
-      <div className="wordproof-getting-started">
-        <h1>Hello</h1>
+      <div className="wordproof-getting-started container bg-white rounded p-4 mt-6">
+
+        <img className="wordproof-logo border-none mx-auto relative" src="https://via.placeholder.com/100" />
+
+        <h1 className="text-lg text-center">Welcome to WordProof</h1>
+        <p className="text-lg text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet.</p>
+
+        <img className="border-none my-8" src="https://via.placeholder.com/150" />
+
+        <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+          Button
+        </button>
+        <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border-b-4 border-gray-400 rounded">
+          Button
+        </button>
+
       </div>
     )
   }

3f97fdaf168a8fd5c21130542b72d4bae8a19588
Marijn Bent <marijn@marijnbent.nl>
2019-09-26 14:07:44 +0200

    Implement PurgeCSS to create smaller css files

--- wordproof-timestamp/postcss.config.js
+++ wordproof-timestamp/postcss.config.js
@@ -1,6 +1,18 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  content: [
+    './src/**/*.js',
+  ],
+
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
-    require('autoprefixer')
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
   ]
 }

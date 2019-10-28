f491d230e0c826a43ca44454b7366af115999e2f
Marijn Bent <marijn@marijnbent.nl>
2019-10-28 14:12:09 +0100

    Use px instead of rem

--- wordproof-timestamp/tailwind.config.js
+++ wordproof-timestamp/tailwind.config.js
@@ -13,8 +13,49 @@ module.exports = {
                 darkblue: '#0017B1'
             }
         },
-        corePlugins: {
-            preflight: true,
-        }
+        spacing: {
+            px: '1px',
+            '0': '0',
+            '1': '4px',
+            '2': '8px',
+            '3': '12px',
+            '4': '16px',
+            '5': '20px',
+            '6': '24px',
+            '8': '32x',
+            '10': '40px',
+            '12': '48px',
+            '16': '64px',
+            '20': '80px',
+            '24': '96px',
+            '32': '128px',
+            '40': '160px',
+            '48': '192px',
+            '56': '224px',
+            '64': '256px',
+        },
+        fontSize: {
+            'xs': '12px',
+            'sm': '14px',
+            'base': '16px',
+            'lg': '18px',
+            'xl': '20px',
+            '2xl': '24px',
+            '3xl': '30px',
+            '4xl': '36px',
+            '5xl': '48px',
+            '6xl': '64px',
+        },
+        maxWidth: {
+            '3xl': '768px',
+            '4xl': '896px',
+        },
+        borderRadius: {
+            'none': '0',
+            'sm': '2px',
+            default: '4px',
+            'lg': '8px',
+            'full': '9999px',
+        },
     }
 };

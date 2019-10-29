49e2a4d53e070462383a1352ccb7b76a76593106
Marijn Bent <marijn@marijnbent.nl>
2019-10-29 17:48:44 +0100

    Fix styling explanation pages

--- wordproof-timestamp/src/js/components/Certficate/Modal/Compare/CompareExplanation.js
+++ wordproof-timestamp/src/js/components/Certficate/Modal/Compare/CompareExplanation.js
@@ -11,16 +11,18 @@ export default class CompareExplanation extends React.Component {
     render() {
         return (
             <>
-                <Nav title={wStrings.importance.nav} backView={'compare'}/>
+                <Nav  title={ wStrings.importance.nav } backView={'compare'} />
 
                 <div className={'flex flex-row'}>
-                    <div className={'w-1/3 overflow-hidden'}>
+                    <div className={'hidden md:block md:w-1/3 overflow-hidden'}>
                         <Locks secure={this.props.valid}/>
                     </div>
-                    <div className={'w-2/3 py-4 text-left px-6'}>
-                        <h2>{ wStrings.importance.title }</h2>
-                        <p>{ wStrings.importance.text }</p>
-                        <button className={'btn btn-primary mt-4'} onClick={this.click}>{ wStrings.importance.cta }</button>
+                    <div className={'w-full md:w-2/3 p-2 md:py-4 md:px-6 text-left'}>
+                        <div className={'md:py-4 md:px-6 text-left border-2 border-gray-300 rounded-lg'}>
+                            <h2>{ wStrings.importance.title }</h2>
+                            <p>{ wStrings.importance.text }</p>
+                            <button className={'btn btn-primary mt-4'} onClick={this.click}>{ wStrings.importance.cta }</button>
+                        </div>
                     </div>
                 </div>
             </>
--- wordproof-timestamp/src/js/components/Certficate/Modal/Overview/OverviewImportance.js
+++ wordproof-timestamp/src/js/components/Certficate/Modal/Overview/OverviewImportance.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import Nav from "../components/Nav";
 import Locks from "./Locks";
+import BlockIntegrity from "./BlockIntegrity";
+import BlockLastEdit from "./BlockLastEdit";
 
 export default class OverviewImportance extends React.Component {
 
     click() {
-        //TODO
+        document.dispatchEvent(new Event('wordproof.modal.navigate.overview'));
     }
 
     render() {
         return (
             <>
-                <Nav title={ wStrings.importance.nav } backView={'overview'}/>
+                <Nav  title={ wStrings.importance.nav } backView={'overview'} />
 
                 <div className={'flex flex-row'}>
-                    <div className={'w-1/3 overflow-hidden'}>
+                    <div className={'hidden md:block md:w-1/3 overflow-hidden'}>
                         <Locks secure={this.props.valid}/>
                     </div>
-                    <div className={'w-2/3 py-4 text-left px-6'}>
-                        <h2>{ wStrings.importance.title }</h2>
-                        <p>{ wStrings.importance.text }</p>
-                        <button className={'btn btn-primary mt-4'} onClick={this.click}>{ wStrings.importance.cta }</button>
+                    <div className={'w-full md:w-2/3 p-2 md:py-4 md:px-6 text-left'}>
+                        <div className={'md:py-4 md:px-6 text-left border-2 border-gray-300 rounded-lg'}>
+                            <h2>{ wStrings.importance.title }</h2>
+                            <p>{ wStrings.importance.text }</p>
+                            <button className={'btn btn-primary mt-4'} onClick={this.click}>{ wStrings.importance.cta }</button>
+                    </div>
                     </div>
                 </div>
             </>

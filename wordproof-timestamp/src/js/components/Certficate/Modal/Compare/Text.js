936ab8b67fe0fc6a50d4d7698883b1b7e7856d3c
Marijn Bent <marijn@marijnbent.nl>
2019-11-15 13:23:01 +0100

    add word-break css

--- wordproof-timestamp/src/js/components/Certficate/Modal/Compare/Text.js
+++ wordproof-timestamp/src/js/components/Certficate/Modal/Compare/Text.js
@@ -5,9 +5,9 @@ export default class Text extends React.Component {
         return (
             <div className={'w-full max-w-full py-5 px-4 rounded-lg border-2 border-gray-300 overflow-y-scroll'} style={{'maxHeight': '280px'}}>
                 {this.props.raw ? (
-                    <div className={'w-full'}>{this.props.text}</div>
+                    <div className={'w-full break-all'}>{this.props.text}</div>
                 ) : (
-                    <div className={'w-full'} dangerouslySetInnerHTML={{__html: this.props.text}}/>
+                    <div className={'w-full break-all'} dangerouslySetInnerHTML={{__html: this.props.text}}/>
                 )}
             </div>
         );

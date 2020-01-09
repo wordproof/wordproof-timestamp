06203f721b66ba5a2bd8c1a35e405a7573aa2ed6
Marijn Bent <marijn@marijnbent.nl>
2020-01-09 13:43:42 +0100

    New mode selection and fix manual mode

--- wordproof-timestamp/src/js/components/Wizard/Steps/Step1.js
+++ wordproof-timestamp/src/js/components/Wizard/Steps/Step1.js
@@ -7,26 +7,21 @@ export default class Step1 extends Component {
     super(props);
   }
 
+  setManual() {
+    this.props.update(null, 'wordproof_wallet_connected', true);
+    this.props.update(null, 'wsfy_is_active', false);
+    setTimeout(function () {
+      window.location.href = wordproof.urls.settings;
+    }, 1000);
+  }
+
   render() {
     return (
       <div className="wordproof-wizard-step">
         <Intro title="Welcome to WordProof Timestamp!" subtitle="Let&apos;s get you set up!" />
 
-        <strong className={'mb-2 block'}>Which mode do you want to use?</strong>
-        <div className="flex flex-row">
-          <div className="border-2 rounded shadow-lg bg-purple-600 text-center p-4 mr-4 w-1/2 text-white">
-            <h2 className="font-semibold my-2 text-white">Manual</h2>
-            <span className={'italic'}>tech-savvy</span>
-            <p>
-              Your own blockchain account & wallet<br/>
-              Set-up in 15 minutes
-            </p>
-
-            <a className="wbtn inline-block mt-3" href={wordproof.urls.manual}>
-              Start Configuration
-            </a>
-          </div>
-
+        <strong className={'mb-2 block text-center'}>Which mode do you want to use?</strong>
+        <div className="flex flex-row justify-center">
           <div className="border-2 rounded shadow-lg bg-blue-600 text-center p-4 ml-4 w-1/2 text-white">
             <h2 className="font-semibold my-2 text-white">Automatic</h2>
             <span className={'italic'}>recommended</span>
@@ -36,10 +31,13 @@ export default class Step1 extends Component {
             </p>
 
             <button className="wbtn inline-block mt-3" onClick={this.props.nextStep}>
-              Get Started
+              Start the set-up
             </button>
           </div>
         </div>
+
+        Advanced: <a onClick={() => this.setManual()} className="inline-block mt-6 cursor-pointer">I want to timestamp manually with my own blockchain account and Scatter</a>
+
       </div>
     );
   }

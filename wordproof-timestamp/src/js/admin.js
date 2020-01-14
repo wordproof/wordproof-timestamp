b3050b41c69e558fd5f604e3e6e1522371b2545f
Marijn Bent <marijn@marijnbent.nl>
2020-01-14 15:48:49 +0100

    Remove old code replaced bu Bulk.js

--- wordproof-timestamp/src/js/admin.js
+++ wordproof-timestamp/src/js/admin.js
@@ -23,70 +23,10 @@ if (document.querySelectorAll('#wordproof-post-widget')) {
 }
 
 /**
- *
+ * Timestamp Button
  */
 if (document.querySelectorAll('.wordproof-timestamp-button')) {
   document.querySelectorAll('.wordproof-timestamp-button').forEach((element) => {
     ReactDOM.render(<TimestampButton automate={element.dataset.automate} post={JSON.parse(decodeURIComponent(element.dataset.post))} meta={JSON.parse(decodeURIComponent(element.dataset.meta))}/>, element);
   })
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-  initAutoStamper();
-});
-
-function initAutoStamper() {
-  let submit = document.querySelector('.wordproof-auto-stamp-submit');
-  if (submit) {
-    submit.addEventListener('click', startAutoStamper);
-  }
-}
-
-async function autoStampSave(postId) {
-  console.log('Saving post ' + postId);
-  var response = await savePost(postId);
-  console.log(response);
-  if (response.errors) {
-    document.querySelector('.wordproof-wsfy-message-' + postId).innerHTML = 'Something went wrong. ' + JSON.stringify(response.errors);
-  } else if (response.success) {
-    document.querySelector('.wordproof-wsfy-message-' + postId).innerHTML = '✅ Post sent to My WordProof';
-  } else if (response.message) {
-    document.querySelector('.wordproof-wsfy-message-' + postId).innerHTML = 'Something went wrong. ' + JSON.stringify(response.message);
-  }
-}
-
-function startAutoStamper(e) {
-  e.preventDefault();
-  e.target.style.display = 'none';
-  console.log('Start');
-  let statusList = document.querySelector('.wordproof-status-list');
-  let status = document.querySelector('.wordproof-status');
-  let statusLeft = document.querySelector('.wordproof-status-left');
-  status.innerHTML = '<img style="width: 30px; height: 30px;" src="' + wordproofAutoStamp.loading + '" />';
-
-  const posts = wordproofAutoStamp.posts;
-  let postsTotal = posts.length;
-  let postsLeft = posts.length;
-
-  let index = 0;
-  let interval = setInterval(function () {
-    const postId = posts[index].ID;
-    const postTitle = posts[index].post_title;
-
-    let item = document.createElement('li');
-    item.innerHTML = 'Saving <strong>' + postTitle + ' (' + postId+ ')  </strong>' +
-      '<span class="wordproof-wsfy-message-' + postId + '"></span>' +
-      '</li>';
-    statusList.appendChild(item);
-
-    statusLeft.innerHTML = postsLeft-- + '/' + postsTotal + ' posts left';
-    autoStampSave(postId);
-
-    index++;
-    if (index === posts.length) {
-      status.innerHTML = '🙌 All done!';
-      statusLeft.innerHTML = '';
-      clearInterval(interval);
-    }
-  }, 2000)
 }

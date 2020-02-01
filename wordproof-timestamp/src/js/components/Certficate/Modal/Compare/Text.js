cb428b4481327e8afee31d8dd93b3f56091d7773
Marijn Bent <marijn@marijnbent.nl>
2020-02-01 14:10:18 +0100

    Fix space char issue

--- wordproof-timestamp/src/js/components/Certficate/Modal/Compare/Text.js
+++ wordproof-timestamp/src/js/components/Certficate/Modal/Compare/Text.js
@@ -2,14 +2,21 @@ import React from 'react';
 
 export default class Text extends React.Component {
     render() {
+        console.log(this.props.text);
         return (
-            <div className={'w-full max-w-full py-5 px-4 rounded-lg border-2 border-gray-300 overflow-y-scroll'} style={{'maxHeight': '280px'}}>
-                {this.props.raw ? (
-                    <div className={'w-full break-all'}>{this.props.text}</div>
-                ) : (
+            this.props.raw ? (
+                <textarea
+                    className={'w-full max-w-full py-5 px-4 rounded-lg border-2 border-gray-300 overflow-y-scroll resize-none'}
+                    style={{'height': '280px'}}>
+                        {this.props.text}
+                    </textarea>
+            ) : (
+                <div className={'w-full max-w-full py-5 px-4 rounded-lg border-2 border-gray-300 overflow-y-scroll'}
+                     style={{'maxHeight': '280px'}}>
                     <div className={'w-full break-all'} dangerouslySetInnerHTML={{__html: this.props.text}}/>
-                )}
-            </div>
+                </div>
+            )
+
         );
     }
 }

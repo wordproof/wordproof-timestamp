ce4b1a1ca55fa67da29ef7a630f6264d299e4603
Marijn Bent <marijn@marijnbent.nl>
2020-05-13 10:47:26 +0200

    Remove eos dev course plug as its discontinued

--- wordproof-timestamp/src/js/components/Settings/Support.js
+++ wordproof-timestamp/src/js/components/Settings/Support.js
@@ -58,28 +58,6 @@ export default class Support extends Component {
                 <h3>Credits</h3>
                 <p><a href="https://wordproof.io/wordpress-plugin-wordproof/credits/" target="_blank"
                       rel="noopener noreferrer">WordProof Timestamp plugin Credits</a></p>
-
-                <h3>Learn</h3>
-                <p>
-                    Are you a WordPress developer? Developing with blockchain technology might spark your interest
-                    too. <a href='https://eos.io/' rel="noopener noreferrer" target="_blank">EOS.IO</a> is open
-                    source blockchain technology.
-                    With WordProof you can timestamp your WordPress content for free in EOS.IO blockchains.
-                </p>
-                <p>
-                    Want to learn how to develop smart contracts and decentralised applications with EOS.IO?
-                    WordProof collaborates with Everything EOS and Cypherglass, to offer the course for free for
-                    WordPress users.
-                </p>
-
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/EUcOY-UpRq0" frameBorder="0"
-                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen></iframe>
-                <p>
-                    Enter the course TODAY via <a href="https://www.everythingeos.io/eos-course-wordproof"
-                                                  rel="noopener noreferrer" target="_blank">this link</a> to get
-                    FREE access as a WordProof user.
-                </p>
             </Template>
         )
     }

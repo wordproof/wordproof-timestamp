51a7aa00ed84e53d97aa21959b948754c95e6575
Marijn Bent <marijn@wordproof.com>
2021-01-21 14:58:02 +0100

    use new ajax call

--- src/js/components/Settings/Bulk.js
+++ src/js/components/Settings/Bulk.js
@@ -13,10 +13,15 @@ export default class Bulk extends Component {
             count: 0,
             running: false,
             done: false,
-            registeredPostTypes: Object.values(wordproofSettings.registeredPostTypes),
+            postTypes: {},
+            loading: false,
         }
 
         this.dismissNotice();
+    }
+
+    componentDidMount() {
+        this.getPostTypeCounts();
     }
 
     dismissNotice() {
@@ -39,10 +44,10 @@ export default class Bulk extends Component {
         this.setState({selected: selected});
     }
 
-    getAmountOfPosts() {
+    getTotalAmountOfSelectedPosts() {
         let amount = 0;
         this.state.selected.map((type) => {
-            amount = amount + (wordproofSettings.bulk.counts[type]).length;
+            amount = amount + this.state.postTypes[type].count;
         });
         return amount;
     }
@@ -58,24 +63,22 @@ export default class Bulk extends Component {
 
         this.setState({running: true});
 
-        let array = [];
+        let posts = [];
         this.state.selected.map((type) => {
-            let n = [];
-            wordproofSettings.bulk.counts[type].map((id) => {
-                n.push({type: type, id: id});
+            this.state.postTypes[type].ids.map((id) => {
+                posts.push({type: type, id: id});
             });
-            array = array.concat(n);
         });
 
         let promise = Promise.resolve();
-        array.forEach((el) => {
+        posts.forEach((el) => {
             promise = promise.then(() => {
 
                 this.sendRequest(el.id, el.type);
                 this.setState({count: this.state.count + 1});
 
                 return new Promise((resolve) => {
-                    setTimeout(resolve, 1000);
+                    setTimeout(resolve, 1500);
                 });
             });
         });
@@ -95,6 +98,26 @@ export default class Bulk extends Component {
         this.addMessage(TimestampButton.retrieveMessage(result, type));
     }
 
+    async getPostTypeCounts() {
+        this.setState({loading: true});
+        const response = await axios.post(wordproofData.ajaxURL, qs.stringify({
+            'action': 'wordproof_get_unprotected_posts',
+            'security': wordproofData.ajaxSecurity
+        }));
+
+        let postTypes = this.state.postTypes;
+        let entries = Object.entries(response.data);
+
+        for (const [type, data] of entries) {
+            postTypes[type] = data;
+        }
+
+        console.log(postTypes);
+
+        this.setState({postTypes: postTypes, loading: false});
+
+    }
+
     render() {
         return (
             <Template current={'bulk'}>
@@ -102,30 +125,44 @@ export default class Bulk extends Component {
                 <table>
                     <tbody>
 
-                    {this.state.registeredPostTypes.map((postType) => (
-                        <tr key={postType}>
-                            <td><input id={'wordproof-bulk-' + postType} onChange={(e) => this.setPostType(e)} value={postType}
-                                       type={'checkbox'}/></td>
-                            <td><label htmlFor={'wordproof-bulk-' + postType}>{postType}</label></td>
-                            <td>{wordproofSettings.bulk.counts[postType].length}</td>
+                    {(this.state.loading)
+                        ? <tr>
+                            <td>Retrieving post types...</td>
                         </tr>
-                    ))}
+                        : Object.entries(this.state.postTypes).map(([postType]) => (
+                            <tr key={postType}>
+                                <td><input id={'wordproof-bulk-' + postType} onChange={(e) => this.setPostType(e)}
+                                           value={postType}
+                                           type={'checkbox'}/></td>
+                                <td><label htmlFor={'wordproof-bulk-' + postType}>{postType}</label></td>
+                                <td>{this.state.postTypes[postType].count}</td>
+                            </tr>
+                        ))
+                    }
 
                     </tbody>
                 </table>
 
-                {(this.state.running) ? <span className={'block mt-4'}>Timestamped {this.state.count } / { this.getAmountOfPosts()} items. Please don&apos;t close this page.</span> : ''}
+                {(this.state.running)
+                    ? <span
+                        className={'block mt-4'}>Timestamped {this.state.count} / {this.getTotalAmountOfSelectedPosts()} items. Please don&apos;t close this page.</span>
+                    : ''
+                }
 
-                <button disabled={this.state.running || this.state.done || this.getAmountOfPosts() > wordproofSettings.balance} onClick={(e) => this.startTimestamping(e)}
-                        className={'wbtn wbtn-primary my-4'}>Start Timestamping
+                <button
+                    disabled={this.state.running || this.state.loading || this.state.done || this.getTotalAmountOfSelectedPosts() > wordproofSettings.balance}
+                    onClick={(e) => this.startTimestamping(e)}
+                    className={'wbtn wbtn-primary my-4'}>Start Timestamping
                 </button>
 
-                {(this.getAmountOfPosts() > wordproofSettings.balance) ? <span className={'block mt-2'}>You do not have enough timestamps. Please <a href={wordproofSettings.urls.upgradeExternal} target="_blank" rel="noopener noreferrer">upgrade your plan</a>.</span> : ''}
+                {(this.getTotalAmountOfSelectedPosts() > wordproofSettings.balance) ?
+                    <span className={'block mt-2'}>You do not have enough timestamps. Please <a
+                        href={wordproofSettings.urls.upgradeExternal} target="_blank" rel="noopener noreferrer">upgrade your plan</a>.</span> : ''}
 
                 <div className={'block'}>
                     {this.state.messages.map((message, key) => (
-                    <span key={key} className={'block mb-2'}>{message}</span>
-                ))}
+                        <span key={key} className={'block mb-2'}>{message}</span>
+                    ))}
                 </div>
 
             </Template>

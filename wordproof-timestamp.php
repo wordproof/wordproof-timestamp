fd023a273e703ad0a8f43f0df40ffb7f65c8a9b5
Marijn Bent <marijn@wordproof.com>
2021-01-30 12:17:29 +0100

    Check if env files exists to remove suppressed error

--- wordproof-timestamp.php
+++ wordproof-timestamp.php
@@ -22,8 +22,10 @@ if (is_readable(__DIR__ . '/vendor/autoload.php')) {
     require __DIR__ . '/vendor/autoload.php';
 
     try {
-        $dotenv = \WordProofTimestamp\Vendor\Dotenv\Dotenv::createImmutable(__DIR__);
-        $dotenv->load();
+    	if (file_exists(__DIR__ . '/.env')) {
+		    $dotenv = \WordProofTimestamp\Vendor\Dotenv\Dotenv::createImmutable( __DIR__ );
+		    $dotenv->load();
+	    }
     } catch(\Exception $e) {}
 
 }

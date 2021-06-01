4e5192b96857e07d3dcf357afe0aaa0e68c664a0
Marijn Bent <marijn@wordproof.com>
2021-06-01 14:33:25 +0200

    ignore eslint

--- src/js/gutenberg/post_meta.js
+++ src/js/gutenberg/post_meta.js
@@ -3,7 +3,10 @@ const { registerPlugin } = wp.plugins;
 import WordProof_Post_Meta from './post_meta_fields';
 
 registerPlugin( 'wordproof-post-settings', {
+
+    /* eslint-disable */
     render() {
         return(<WordProof_Post_Meta />);
     }
+    /* eslint-enable */
 } );

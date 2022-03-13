7f31fd570b303ed215490c20ee69145b58123385
Marijn Bent <marijn@wordproof.com>
2022-03-14 00:30:25 +0700

    add extra data to bulk page

--- app/Controllers/AdminPageController.php
+++ app/Controllers/AdminPageController.php
@@ -2,18 +2,20 @@
 
 namespace WordProofTimestamp\App\Controllers;
 
+use WordProof\SDK\Helpers\PostTypeHelper;
 use WordProofTimestamp\App\Helpers\AssetHelper;
 
 class AdminPageController {
 
-    private $pages = ['admin_page_wordproof-about', 'admin_page_wordproof-migrate'];
+	private $pages = [ 'admin_page_wordproof-about', 'admin_page_wordproof-bulk' ];
 
 	public function __construct() {
 
 		//TODO Display if not shown before.
-		add_action( 'admin_menu', [$this, 'addPages'] );
-		add_action( 'admin_enqueue_scripts', [$this, 'enqueue'] );
-		add_filter( 'wordproof_load_data_on_pages', [$this, 'loadDataOnPageHook']);
+		add_action( 'admin_menu', [ $this, 'addPages' ] );
+		add_action( 'admin_enqueue_scripts', [ $this, 'enqueue' ] );
+		add_filter( 'wordproof_load_data_on_pages', [ $this, 'loadDataOnPageHook' ] );
+		add_filter( 'wordproof_data', [ $this, 'loadExtraData' ] );
 	}
 
 	public function addPages() {
@@ -23,33 +25,57 @@ class AdminPageController {
 			'WordProof' . WORDPROOF_VERSION,
 			'manage_options',
 			'wordproof-about',
-			[$this, 'aboutPageContent'],
+			[ $this, 'aboutPageContent' ],
 		);
 		add_submenu_page(
 			null,
 			'WordProof ' . WORDPROOF_VERSION,
 			'WordProof' . WORDPROOF_VERSION,
 			'manage_options',
-			'wordproof-migrate',
-			[$this, 'migratePageContent'],
+			'wordproof-bulk',
+			[ $this, 'migratePageContent' ],
 		);
 	}
 
-	public function enqueue($page) {
-		if (in_array($page,$this->pages)) {
-			AssetHelper::enqueue('index');
-			AssetHelper::enqueue('main.css');
-        }
-    }
+	public function enqueue( $page ) {
+		if ( in_array( $page, $this->pages ) ) {
+			AssetHelper::enqueue( 'index' );
+			AssetHelper::enqueue( 'main.css' );
+		}
+	}
 
-	public function loadDataOnPageHook($pages) {
-	    return array_merge($pages, $this->pages);
-    }
+	public function loadDataOnPageHook( $pages ) {
+		return array_merge( $pages, $this->pages );
+	}
+
+	public function loadExtraData( $data ) {
+
+		if ( ! is_admin() ) {
+			return $data;
+		}
+
+		$screen = get_current_screen();
+		if ( isset( $screen->base ) && $screen->base === 'admin_page_wordproof-bulk' ) {
+
+			$postTypes     = PostTypeHelper::getPublicPostTypes();
+			$postTypesData = [];
+
+
+			foreach ( $postTypes as $postType ) {
+				$postTypesData[ $postType ] = PostTypeHelper::getUnprotectedPosts( $postType );
+			}
+
+			return ['data' => array_merge( $data['data'], ['post_types' => $postTypesData ] ) ];
+		}
+
+		return $data;
+
+	}
 
 	public function migratePageContent() {
 		?>
 
-		<div id="wordproof-migrate"></div>
+        <div id="wordproof-bulk"></div>
 
 		<?php
 	}
@@ -57,7 +83,7 @@ class AdminPageController {
 	public function aboutPageContent() {
 		?>
 
-		<div id="wordproof-about"></div>
+        <div id="wordproof-about"></div>
 
 		<?php
 	}

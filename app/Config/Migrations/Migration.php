540d204cef9691db1c693581a86361453f5729b2
Marijn Bent <marijn@wordproof.com>
2022-03-13 14:21:29 +0700

    Add v3 migration to prepare for reauthentication and save of settings

---
+++ app/Config/Migrations/Migration.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace WordProofTimestamp\App\Config\Migrations;
+
+/**
+ * Base migration class.
+ */
+abstract class Migration {
+
+	/**
+	 * Performs the migration.
+	 *
+	 * @return void
+	 */
+	abstract public function up();
+
+}
---
+++ app/Config/Migrations/v3Migration.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace WordProofTimestamp\App\Config\Migrations;
+
+use WordProofTimestamp\App\Vendor\WordProof\SDK\Helpers\TransientHelper;
+
+class v3Migration extends Migration {
+
+	public function up() {
+
+		$this->saveAccessToken();
+		$this->saveSettings();
+
+		TransientHelper::set('wordproof_waiting_for_authentication_to_start_post_migration', true);
+		TransientHelper::set('wordproof_migration_300', true);
+
+	}
+
+	private function saveAccessToken() {
+		$oauthData = get_option('wordproof_oauth');
+
+		/**
+		 * Save access token to login user with linked account while going through the new OAuth process.
+		 */
+		if (isset($oauthData) && is_array($oauthData) && isset($oauthData['access_token'])) {
+			TransientHelper::set('wordproof_v2_authenticate_with_token', $oauthData['access_token'] );
+		}
+	}
+
+	private function saveSettings() {
+
+		$wsfyData = get_option('wordproof_wsfy');
+		$hideLink = get_option('wordproof_hide_certificate_home');
+		$linkText = get_option('wordproof_certificate_text');
+
+		if (isset($wsfyData) && is_array($wsfyData)) {
+
+			/**
+			 * Save source id to use the current source while going through the new OAuth process.
+			 */
+			if (isset($wsfyData['site_id'])) {
+				TransientHelper::set('wordproof_v2_get_existing_source', $wsfyData['site_id'] );
+			}
+
+			/**
+			 * Use the current settings.
+			 */
+			$settings = [];
+
+			if (isset($wsfyData['show_revisions'])) {
+				$settings['show_revisions'] = $wsfyData['show_revisions'];
+			}
+
+			if (isset($wsfyData['allowed_post_types'])) {
+				$settings['selected_post_types'] = array_filter($wsfyData['allowed_post_types']);
+			}
+
+			if (isset($linkText)) {
+				$settings['certificate_link_text'] = $linkText;
+			}
+
+			if (isset($hideLink)) {
+				$settings['hide_certificate_link'] = boolval($hideLink);
+			}
+
+			if (!empty($settings)) {
+				update_option('wordproof_settings', (object) $settings);
+			}
+
+		}
+	}
+}

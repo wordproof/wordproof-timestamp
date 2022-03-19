dcbccee2d7fdf9e92ba18b66c894a06567a085e0
Marijn Bent <marijn@wordproof.com>
2022-03-19 16:59:25 +0700

    add config

--- app/Config/SdkAppConfig.php
+++ app/Config/SdkAppConfig.php
@@ -34,16 +34,31 @@ class SdkAppConfig extends DefaultAppConfig
         return DotenvHelper::get('APP_ENV', 'production');
     }
 
+    /**
+     * The OAuth client used.
+     *
+     * @return mixed|null
+     */
     public function getOauthClient()
     {
         return DotenvHelper::get('WORDPROOF_CLIENT', null);
     }
 
+    /**
+     * The WordProof MY url used.
+     *
+     * @return mixed|null
+     */
     public function getWordProofUrl()
     {
         return DotenvHelper::get('WORDPROOF_ENDPOINT', null);
     }
 
+    /**
+     * Override the SDK asset URL's.
+     *
+     * @return mixed|null
+     */
     public function getScriptsFileOverwrite()
     {
         return DotenvHelper::get('WORDPROOF_SDK_OVERWRITE', null);
